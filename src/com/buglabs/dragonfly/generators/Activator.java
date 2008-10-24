3b0ed2cd286810ee41e7e154f2d4000fc0dea4e4
kgilmer <kgilmer@35ad6e01-222d-0410-b96a-b41bba574936>
2008-10-24 14:36:53 +0000

    git-svn-id: svn://bugcamp.net/dragonfly/trunk/com.buglabs.dragonfly.generators@6328 35ad6e01-222d-0410-b96a-b41bba574936

---
+++ src/com/buglabs/dragonfly/generators/Activator.java
@@ -0,0 +1,14 @@
+package com.buglabs.dragonfly.generators;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+public class Activator extends AbstractUIPlugin {
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+	}
+
+	public void stop(BundleContext context) throws Exception {
+		super.stop(context);
+	}
+}
