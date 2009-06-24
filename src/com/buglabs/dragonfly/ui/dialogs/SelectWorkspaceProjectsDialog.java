b631ca70e9297f71257053d0e71e8b630e7dab95
bballantine <bballantine@35ad6e01-222d-0410-b96a-b41bba574936>
2009-06-24 19:58:36 +0000

    com.buglabs.dragonfly.ui - Added Select All/None links to App Selector Dialog
    git-svn-id: svn://bugcamp.net/dragonfly/trunk/com.buglabs.dragonfly.ui@9485 35ad6e01-222d-0410-b96a-b41bba574936

--- src/com/buglabs/dragonfly/ui/dialogs/SelectWorkspaceProjectsDialog.java
+++ src/com/buglabs/dragonfly/ui/dialogs/SelectWorkspaceProjectsDialog.java
@@ -1,10 +1,10 @@
 package com.buglabs.dragonfly.ui.dialogs;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.resource.ColorRegistry;
 import org.eclipse.jface.viewers.CheckStateChangedEvent;
 import org.eclipse.jface.viewers.CheckboxTableViewer;
 import org.eclipse.jface.viewers.ICheckStateListener;
@@ -15,15 +15,18 @@ import org.eclipse.jface.viewers.TableLayout;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.ui.forms.events.HyperlinkEvent;
+import org.eclipse.ui.forms.events.IHyperlinkListener;
+import org.eclipse.ui.forms.widgets.Hyperlink;
 
 import com.buglabs.dragonfly.ui.util.BugProjectSelectionManager;
 import com.buglabs.dragonfly.ui.util.BugProjectUtil;
@@ -38,8 +41,13 @@ import com.buglabs.dragonfly.ui.util.BugProjectUtil;
  */
 public class SelectWorkspaceProjectsDialog extends Dialog {
 
-	private String[] 	bug_project_names;
-	CheckboxTableViewer projects_viewer;
+	private static final String HYPERLINKCOLOR 		= "HYPERLINKCOLOR";
+	private static final String DIALOG_TITLE_TXT	= "Select Applications to Run";
+	private static final String SELECT_ALL_TXT 		= "Select All";
+	private static final String SELECT_NONE_TXT		= "Select None";
+	private static ColorRegistry color_registry = null;
+	private String[] 			bug_project_names;
+	private CheckboxTableViewer projects_viewer;
 	
 	public SelectWorkspaceProjectsDialog(Shell parentShell) {
 		super(parentShell);
@@ -49,8 +57,17 @@ public class SelectWorkspaceProjectsDialog extends Dialog {
 			(String[]) projectNames.toArray(new String[projectNames.size()]);
 	}
 
+	private void createColorRegistry(Composite parent) {
+		if (color_registry == null)
+			color_registry = new ColorRegistry(parent.getDisplay());
+		
+		if (!color_registry.hasValueFor(HYPERLINKCOLOR))
+			color_registry.put(HYPERLINKCOLOR, new RGB(98,83,125));
+	}
+	
 	@Override
 	protected Control createDialogArea(Composite parent) {
+		createColorRegistry(parent);
 		
 		Composite projectsControl = new Composite(parent, SWT.NONE);
 		GridData gdProjects = new GridData(GridData.FILL_BOTH);
@@ -58,7 +75,7 @@ public class SelectWorkspaceProjectsDialog extends Dialog {
 		projectsControl.setLayout(new GridLayout(1, false));
 		
 		Label label = new Label(projectsControl, SWT.TOP);
-		label.setText("Select Applications to Run");
+		label.setText(DIALOG_TITLE_TXT);
 		
 		Table projectsTable = new Table(projectsControl ,SWT.CHECK | SWT.BORDER | SWT.V_SCROLL);
 		projectsTable.setHeaderVisible(false);
@@ -83,10 +100,8 @@ public class SelectWorkspaceProjectsDialog extends Dialog {
 		// get the stored state of selected elements or select all
 		String[] selectedProjects = 
 			BugProjectSelectionManager.getInstance().getSelectedProjectNames();
-		if (selectedProjects == null)
-			projects_viewer.setAllChecked(true);
-		else 
-			projects_viewer.setCheckedElements(selectedProjects);
+		if (selectedProjects == null) selectAll();
+		else projects_viewer.setCheckedElements(selectedProjects);
 		
 		// update the selection manager when things checked/unchecked
 		projects_viewer.addCheckStateListener(new ICheckStateListener() {
@@ -100,9 +115,62 @@ public class SelectWorkspaceProjectsDialog extends Dialog {
 			}
 		});
 		
+		// draw select all/none hyperlinks
+		Composite hyperLinkHolder = new Composite(projectsControl, SWT.NONE);
+		
+	    GridLayout layoutHL = new GridLayout(2, false);
+	    layoutHL.marginTop = layoutHL.marginBottom = 0;
+	    layoutHL.verticalSpacing = 0;
+	    layoutHL.marginRight = 5;
+		hyperLinkHolder.setLayout(layoutHL);
+		GridData gdHL = new GridData(SWT.CENTER, SWT.CENTER, true, false);
+		gdHL.horizontalAlignment = SWT.FILL;
+		hyperLinkHolder.setLayoutData(gdHL);	
+		
+		Hyperlink selectAllLink = new Hyperlink(hyperLinkHolder, SWT.LEFT);
+		selectAllLink.setText(SELECT_ALL_TXT);
+		selectAllLink.setToolTipText(SELECT_ALL_TXT);
+		selectAllLink.setUnderlined(true);
+		selectAllLink.setForeground(color_registry.get(HYPERLINKCOLOR));
+		selectAllLink.addHyperlinkListener(new SelectionHyperlinkListener() {
+			public void linkActivated(HyperlinkEvent e) {
+				selectAll();
+			}
+		});
+
+		Hyperlink selectNoneLink = new Hyperlink(hyperLinkHolder, SWT.RIGHT);
+		selectNoneLink.setText(SELECT_NONE_TXT);
+		selectNoneLink.setToolTipText(SELECT_NONE_TXT);
+		selectNoneLink.setUnderlined(true);
+		selectNoneLink.setForeground(color_registry.get(HYPERLINKCOLOR));
+		selectNoneLink.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, true, false));
+		selectNoneLink.addHyperlinkListener(new SelectionHyperlinkListener() {
+			public void linkActivated(HyperlinkEvent e) {
+				selectNone();
+			}
+		});
+		
 		return projectsControl;
 	}
 
+	private void selectAll() {
+		projects_viewer.setAllChecked(true);
+		BugProjectSelectionManager.getInstance().setSelectedProjectNames(bug_project_names);
+	}
+	
+	private void selectNone() {
+		projects_viewer.setAllChecked(false);
+		BugProjectSelectionManager.getInstance().setSelectedProjectNames(new String[0]);
+	}
+	
+	
+	abstract class SelectionHyperlinkListener implements IHyperlinkListener {
+		// not needed for this functionality
+		public void linkEntered(HyperlinkEvent e) {}
+		public void linkExited(HyperlinkEvent e) {}
+		
+	}
+	
 	class ProjectsContentProvider implements IStructuredContentProvider {
 		public Object[] getElements(Object input) {
 			if (input instanceof String[]) {
@@ -131,6 +199,6 @@ public class SelectWorkspaceProjectsDialog extends Dialog {
 		public Image getColumnImage(Object element, int columnIndex) {
 			return null;
 		}
-	}	
+	}
 	
 }
