32eeac394733eb5a9cb6439324244e5e6c801246
bballantine <bballantine@35ad6e01-222d-0410-b96a-b41bba574936>
2009-11-17 16:22:57 +0000

    com.buglabs.dragonfly.ui - had a weird import that eclipse must of added that was killing the build.  removed unused import.
    git-svn-id: svn://bugcamp.net/dragonfly/trunk/com.buglabs.dragonfly.ui@10514 35ad6e01-222d-0410-b96a-b41bba574936

--- src/com/buglabs/dragonfly/ui/actions/ExportJarToBUGNetAction.java
+++ src/com/buglabs/dragonfly/ui/actions/ExportJarToBUGNetAction.java
@@ -29,7 +29,6 @@ import com.buglabs.dragonfly.exception.BugnetAuthenticationException;
 import com.buglabs.dragonfly.util.UIUtils;
 import com.buglabs.dragonfly.validator.BUGApplicationProjectValidator;
 import com.buglabs.osgi.concierge.core.utils.ProjectUtils;
-import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
 
 public class ExportJarToBUGNetAction extends Action {
 
