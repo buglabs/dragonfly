9fd63b6a3de133c98087222695cdfadf8e73aff3
kgilmer <kgilmer@35ad6e01-222d-0410-b96a-b41bba574936>
2010-07-27 17:48:00 +0000

    com.buglabs.bug.simulator: fixed shutdown issue and build metadata.
    git-svn-id: svn://bugcamp.net/dragonfly/trunk/com.buglabs.bug.simulator@11543 35ad6e01-222d-0410-b96a-b41bba574936

--- com/buglabs/bug/module/lcd/LCDActivator.java
+++ com/buglabs/bug/module/lcd/LCDActivator.java
@@ -14,9 +14,10 @@ import com.buglabs.bug.module.pub.IModletFactory;
 import com.buglabs.util.LogServiceUtil;
 
 /**
- * Activator for the LCD bundle (simulator).  
+ * Activator for the LCD bundle (simulator).
+ * 
  * @author kgilmer
- *
+ * 
  */
 public class LCDActivator implements BundleActivator, IModletFactory {
 
@@ -27,23 +28,23 @@ public class LCDActivator implements BundleActivator, IModletFactory {
 	private ServiceRegistration sr;
 
 	private static LCDActivator instance;
-	
+
 	public LCDActivator() {
 		instance = this;
 	}
 
 	public void start(BundleContext context) throws Exception {
 		this.context = context;
-		
+
 		Dictionary dict = new Hashtable();
 		dict.put("Modlet Provider", getName());
 		dict.put("Module", getModuleId());
-		
+
 		sr = context.registerService(IModletFactory.class.getName(), this, dict);
 		logService = LogServiceUtil.getLogService(context);
-		
+
 	}
-	
+
 	public static LCDActivator getInstance() {
 		synchronized (instance) {
 			return instance;
@@ -51,18 +52,21 @@ public class LCDActivator implements BundleActivator, IModletFactory {
 	}
 
 	public void stop(BundleContext context) throws Exception {
-		sr.unregister();
+		try {
+			sr.unregister();
+		} catch (IllegalStateException e) {
+			//Ignore this error
+		}
 	}
 
 	public IModlet createModlet(BundleContext context, int slotId) {
 		return new LCDModlet(context, slotId, getModuleId(), logService);
 	}
 
-
 	public IModlet createModlet(BundleContext context, int slotId, BMIModuleProperties properties) {
 		return createModlet(context, slotId);
-	}	
-	
+	}
+
 	public String getModuleId() {
 		return "LCD";
 	}
