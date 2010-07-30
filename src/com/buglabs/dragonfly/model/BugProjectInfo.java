031fa95251168af6fe014b302120a0dacfcb55cd
kgilmer <kgilmer@35ad6e01-222d-0410-b96a-b41bba574936>
2010-07-30 20:48:29 +0000

    com.buglabs.dragonfly: remove CTE.ui dependency (feature)
    git-svn-id: svn://bugcamp.net/dragonfly/trunk/com.buglabs.dragonfly@11616 35ad6e01-222d-0410-b96a-b41bba574936

---
+++ src/com/buglabs/dragonfly/model/BugProjectInfo.java
@@ -0,0 +1,137 @@
+package com.buglabs.dragonfly.model;
+
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+
+public class BugProjectInfo {
+	private String projectName;
+	private String version;
+	private String activator;
+	private String vendor;
+	private String symbolicName;
+	private String executionEnvironment;
+	private boolean generateActivator;
+	boolean shouldGenerateApplicationLoop;
+
+	private Vector services;
+	private Hashtable<String, List<ServicePropertyHelper>> service_property_helpers;
+
+	public BugProjectInfo() {
+		version = "1.0.0";
+		vendor = "";
+		symbolicName = "";
+		activator = "";
+		projectName = "";
+		generateActivator = true;
+		executionEnvironment = "";
+		
+		shouldGenerateApplicationLoop = false;
+		services = new Vector();
+		service_property_helpers = new Hashtable<String, List<ServicePropertyHelper>>();
+	}
+
+	public Vector getServices() {
+		return services;
+	}
+
+	public Map<String, List<ServicePropertyHelper>> getServicePropertyHelperMap() {
+		return service_property_helpers;
+	}
+
+	public boolean isShouldGenerateApplicationLoop() {
+		return shouldGenerateApplicationLoop;
+	}
+
+	public void setShouldGenerateApplicationLoop(boolean shouldGenerateApplicationLoop) {
+		this.shouldGenerateApplicationLoop = shouldGenerateApplicationLoop;
+	}
+	
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+
+	public String getActivator() {
+		return activator;
+	}
+
+	public void setActivator(String activator) {
+		this.activator = activator;
+	}
+
+	public String getSymbolicName() {
+		return symbolicName;
+	}
+
+	public void setSymbolicName(String symbolicName) {
+		this.symbolicName = symbolicName;
+	}
+
+	public String getVendor() {
+		return vendor;
+	}
+
+	public void setVendor(String vendor) {
+		this.vendor = vendor;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	public boolean isGenerateActivator() {
+		return generateActivator;
+	}
+
+	public void setGenerateActivator(boolean generateActivator) {
+		this.generateActivator = generateActivator;
+	}
+
+	public String getActivatorName() {
+		if (getActivator().equals("")) {
+			return "";
+		}
+
+		String name = getActivator();
+		int index = getActivator().lastIndexOf(".");
+
+		if (index > 0 && index < name.length()) {
+			name = name.substring(index + 1, name.length());
+		}
+
+		return name;
+	}
+
+	public String getActivatorPackage() {
+		if (getActivator().equals("")) {
+			return "";
+		}
+
+		String name = getActivator();
+
+		int index = name.lastIndexOf(".");
+		if (index > 0) {
+			name = name.substring(0, index);
+		}
+
+		return name.toLowerCase();
+	}
+
+	public String getExecutionEnvironment() {
+		return executionEnvironment;
+	}
+
+	public void setExecutionEnvironment(String executionEnvironment) {
+		this.executionEnvironment = executionEnvironment;
+	}
+}
