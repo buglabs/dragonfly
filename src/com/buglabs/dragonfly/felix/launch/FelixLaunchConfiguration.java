80da27d66e62841252d8bcca7bdc08f24ab094fc
kgilmer <kgilmer@35ad6e01-222d-0410-b96a-b41bba574936>
2010-10-25 16:01:51 +0000

    com.buglabs.dragonfly.felix: updated felix version in launch configuration.
    git-svn-id: svn://bugcamp.net/dragonfly/trunk/com.buglabs.dragonfly.felix@12302 35ad6e01-222d-0410-b96a-b41bba574936

--- src/com/buglabs/dragonfly/felix/launch/FelixLaunchConfiguration.java
+++ src/com/buglabs/dragonfly/felix/launch/FelixLaunchConfiguration.java
@@ -48,7 +48,7 @@ public abstract class FelixLaunchConfiguration extends LaunchConfigurationDelega
 
 	private static final String FELIX_MAIN_CLASS = "org.apache.felix.main.Main";
 	private static final String REL_BUNDLE_DIR = "bundle";
-	private static final String FELIX_FRAMEWORK_REL_PATH = "framework" + File.separator + "org.apache.felix.main-3.0.1.jar";
+	private static final String FELIX_FRAMEWORK_REL_PATH = "framework" + File.separator + "org.apache.felix.main-3.0.4.jar";
 	private IPath launchDir;
 	private boolean debug = true;
 	
