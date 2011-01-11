b05f6ce7dcddf79834ad83aae99d6eb891fe132d
kgilmer <kgilmer@35ad6e01-222d-0410-b96a-b41bba574936>
2011-01-11 09:12:58 +0000

    com.buglabs.bug.dragonfly.generators: Changes to templates to support R2.x ST framework.
    git-svn-id: svn://bugcamp.net/dragonfly/trunk/com.buglabs.dragonfly.generators@12814 35ad6e01-222d-0410-b96a-b41bba574936

--- src/com/buglabs/osgi/concierge/templates/GeneratorActivator.java
+++ src/com/buglabs/osgi/concierge/templates/GeneratorActivator.java
@@ -2,39 +2,38 @@ package com.buglabs.osgi.concierge.templates;
 
 import com.buglabs.dragonfly.model.BugProjectInfo;
 
-public class GeneratorActivator {
-	protected static String nl;
+ public class GeneratorActivator
+ {
+  protected static String nl;
+  public static synchronized GeneratorActivator create(String lineSeparator)
+  {
+    nl = lineSeparator;
+    GeneratorActivator result = new GeneratorActivator();
+    nl = null;
+    return result;
+  }
 
-	public static synchronized GeneratorActivator create(String lineSeparator) {
-		nl = lineSeparator;
-		GeneratorActivator result = new GeneratorActivator();
-		nl = null;
-		return result;
-	}
+  public final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl;
+  protected final String TEXT_1 = "package ";
+  protected final String TEXT_2 = ";";
+  protected final String TEXT_3 = NL + NL + "import org.osgi.framework.BundleActivator;" + NL + "import org.osgi.framework.BundleContext;" + NL + "" + NL + "public class ";
+  protected final String TEXT_4 = " implements BundleActivator {" + NL + "" + NL + "\tpublic void start(BundleContext context) throws Exception {" + NL + "\t\t// TODO Auto-generated method stub" + NL + "\t\t" + NL + "\t}" + NL + "" + NL + "\tpublic void stop(BundleContext context) throws Exception {" + NL + "\t\t// TODO Auto-generated method stub" + NL + "\t\t" + NL + "\t}" + NL + "}";
 
-	public final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl;
-	protected final String TEXT_1 = "package ";
-	protected final String TEXT_2 = ";";
-	protected final String TEXT_3 = NL + NL + "import org.osgi.framework.BundleActivator;" + NL + "import org.osgi.framework.BundleContext;" + NL + "" + NL + "public class ";
-	protected final String TEXT_4 = " implements BundleActivator {" + NL + "" + NL + "\tpublic void start(BundleContext context) throws Exception {" + NL
-			+ "\t\t// TODO Auto-generated method stub" + NL + "\t\t" + NL + "\t}" + NL + "" + NL + "\tpublic void stop(BundleContext context) throws Exception {" + NL
-			+ "\t\t// TODO Auto-generated method stub" + NL + "\t\t" + NL + "\t}" + NL + "}";
+   public String generate(BugProjectInfo projInfo)
+  {
+    final StringBuffer stringBuffer = new StringBuffer();
+    
+if(projInfo.getActivatorPackage().length() > 0) {
 
-	public String generate(BugProjectInfo projInfo) {
-		final StringBuffer stringBuffer = new StringBuffer();
-		//Author: Angel Roman - roman@mdesystems.com
+    stringBuffer.append(TEXT_1);
+    stringBuffer.append(projInfo.getActivatorPackage());
+    stringBuffer.append(TEXT_2);
+    
+}
 
-		if (projInfo.getActivatorPackage().length() > 0) {
-
-			stringBuffer.append(TEXT_1);
-			stringBuffer.append(projInfo.getActivatorPackage());
-			stringBuffer.append(TEXT_2);
-
-		}
-
-		stringBuffer.append(TEXT_3);
-		stringBuffer.append(projInfo.getActivatorName());
-		stringBuffer.append(TEXT_4);
-		return stringBuffer.toString();
-	}
+    stringBuffer.append(TEXT_3);
+    stringBuffer.append(projInfo.getActivatorName());
+    stringBuffer.append(TEXT_4);
+    return stringBuffer.toString();
+  }
 }
