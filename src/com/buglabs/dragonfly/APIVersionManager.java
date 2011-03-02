5b1679716bb90e45ef94023d6dc8613bba4c4187
kgilmer <kgilmer@35ad6e01-222d-0410-b96a-b41bba574936>
2011-03-02 00:42:35 +0000

    com.buglabs.dragonfly: just return hardcoded string for BUG API version, and do not try to get fancy with bundle manifests. fixes #1912
    git-svn-id: svn://bugcamp.net/dragonfly/trunk/com.buglabs.dragonfly@12979 35ad6e01-222d-0410-b96a-b41bba574936

--- src/com/buglabs/dragonfly/APIVersionManager.java
+++ src/com/buglabs/dragonfly/APIVersionManager.java
@@ -7,34 +7,20 @@
  *******************************************************************************/
 package com.buglabs.dragonfly;
 
+/**
+ * Return the version string for the BUG API generated project was designed for.
+ *
+ */
 public class APIVersionManager {
 
 	public static final String BUG_API_VERSION_MANIFEST_KEY = "BUG-API-Version";
 
 	/**
-	 * This will only be set on some sort of error. This is the first version to
-	 * contain this functionality
+	 * Change upon every API-changing BUG sw release.
 	 */
-	private static final String DEFAULT_API_VERSION = "2.0.1";
-	private static String sdk_api_version = null;
+	private static final String DEFAULT_API_VERSION = "2.0.2";
 
-	public static String getSDKAPIVersion() {
-		if (sdk_api_version == null) {
-			synchronized (APIVersionManager.class) {
-				if (sdk_api_version == null) {
-					sdk_api_version = getSDKAPIVersionFromManifest();
-				}
-			}
-		}
-
-		if (sdk_api_version == null || sdk_api_version.length() == 0)
-			sdk_api_version = DEFAULT_API_VERSION;
-
-		return sdk_api_version;
+	public static String getSDKAPIVersion() {		
+		return DEFAULT_API_VERSION;
 	}
-
-	private static String getSDKAPIVersionFromManifest() {
-		return (String) DragonflyActivator.getDefault().getBundle().getHeaders().get(BUG_API_VERSION_MANIFEST_KEY);
-	}
-
 }
