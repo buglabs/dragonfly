51678f68d5416e481dd10f9e37a28c0e8e3e2eb8
Ken Gilmer <kgilmer@gmail.com>
2011-07-28 16:51:42 +0900

    dragonfly: code cleanup in the generators bundle.

--- com.buglabs.dragonfly.generators/src/com/buglabs/dragonfly/generators/Activator.java
+++ com.buglabs.dragonfly.generators/src/com/buglabs/dragonfly/generators/Activator.java
@@ -3,11 +3,22 @@ package com.buglabs.dragonfly.generators;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
+/**
+ * Activator for generators plugin.
+ *
+ */
 public class Activator extends AbstractUIPlugin {
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 	}
 
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
 	public void stop(BundleContext context) throws Exception {
 		super.stop(context);
 	}
