6bcbd257f415a5a018a506ca5f79a5402325f3ac
Ken Gilmer <kgilmer@gmail.com>
2011-08-01 21:29:35 +0900

    dragonfly: cannot rely on build system to set file permissions due to crossplatform problems with Zip.  Simulator plugin will manually set correct permissions on files on plugin.start().

--- com.buglabs.dragonfly.simulator/src/com/buglabs/dragonfly/simulator/Activator.java
+++ com.buglabs.dragonfly.simulator/src/com/buglabs/dragonfly/simulator/Activator.java
@@ -13,8 +13,23 @@ import org.eclipse.core.runtime.FileLocator;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
+/**
+ * Activator for simulator.
+ *
+ */
 public class Activator implements BundleActivator {
 
+	/**
+	 * Bundles that should be started when the simulator starts.
+	 */
+	private static final String[] STARTABLE_BUNDLES = {
+				"com.buglabs.app.bugdash2.jar",
+				"com.buglabs.bug.dragonfly.jar",
+				"com.buglabs.bug.simulator.jar",
+				"com.buglabs.bug.ws.jar",
+				"com.buglabs.osgi.sewing.jar",
+				"com.buglabs.util.shell.jar"};
+	
 	private static BundleContext context;
 	private static Activator ref;
 
@@ -32,6 +47,32 @@ public class Activator implements BundleActivator {
 	 */
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
+		resetBundlePermissions(getBUGBundleLocation());
+	}
+
+	/**
+	 * Set the exec permission on startable BUG bundles.  This would preferably be done in the build but the Eclipse PDE build system does not
+	 * allow for setting permissions on files due to a limitation with Zip.
+	 * 
+	 * @param bundleDir
+	 * @throws IOException
+	 */
+	private void resetBundlePermissions(String bundleDir) throws IOException {
+		File dir = new File(bundleDir);
+		
+		if (!dir.exists() || dir.isFile())
+			throw new IOException("Invalid bundle directory: " + bundleDir);
+		
+		List<String> execBundles = Arrays.asList(STARTABLE_BUNDLES);
+		
+		for (String name : execBundles) {
+			File bf = new File(dir, name);
+			
+			if (!bf.exists() || !bf.isFile())
+				throw new IOException("Unable to find " + name);
+			
+			bf.setExecutable(true);
+		}
 	}
 
 	/*
